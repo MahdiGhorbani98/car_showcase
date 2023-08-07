3dc940ae3c7330c3e4859e3bc684e72c248794e1
oliver <mahdig502@gmail.com>
2023-08-07 12:06:56 +0330

    Navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,22 +5,20 @@ import Image from "next/image";
 const Hero = () => {
   const handleScroll = () => {};
   return (
-    <div>
-      <div className="hero">
-        <div className="flex-1 pt-36 padding-x">
-          <h1 className="hero__title">
-            Find, book, rent a car—quick and super easy!
-          </h1>
-          <p className="hero__subtitle">
-            Streamline your car rental experience with our effortless booking
-            process.
-          </p>
-          <CustomButton
-            title="Explore Cars"
-            containerStyles="bg-primary-blue text-white rounded-full mt-10"
-            handleClick={handleScroll}
-          />
-        </div>
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book, rent a car—quick and super easy!
+        </h1>
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
       </div>
       <div className="hero__image-container">
         <div className="hero__image">

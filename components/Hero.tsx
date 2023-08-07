db6d8cf37af0170e9ba217585de5694bf098a515
oliver <mahdig502@gmail.com>
2023-08-07 12:41:13 +0330

    Footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const Hero = () => {
   const handleScroll = () => {};
   return (
-    <div className="hero">
+    <div className="hero ">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
           Find, book, rent a car—quick and super easy!
@@ -23,8 +23,8 @@ const Hero = () => {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
-          <div className="hero__image-overlay"></div>
         </div>
+        <div className="hero__image-overlay"></div>
       </div>
     </div>
   );

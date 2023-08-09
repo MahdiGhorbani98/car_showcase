f2afd17cf912a72a5f97d3f695a4f165a372e502
oliver <mahdig502@gmail.com>
2023-08-09 15:31:13 +0330

    SearchBar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -7,7 +7,7 @@ const SearchBar = () => {
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
-      <div className="searchvar__item">
+      <div className="searchbar__item">
         <SearchManuFacturer
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
--- components/SearchManuFacturer.tsx
+++ components/SearchManuFacturer.tsx
@@ -23,7 +23,7 @@ const SearchManuFacturer = ({
 
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]">
             <Image
@@ -51,8 +51,8 @@ const SearchManuFacturer = ({
             leaveTo="opacity-0"
             afterLeave={() => setQuery("")} // Reset the search query after the transition completes
           >
-            <Combobox.Option
-              className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+            <Combobox.Options
+              className=" mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
               static
             >
               {filteredManufacturers.length === 0 && query !== "" ? (
@@ -98,7 +98,7 @@ const SearchManuFacturer = ({
                   </Combobox.Option>
                 ))
               )}
-            </Combobox.Option>
+            </Combobox.Options>
           </Transition>
         </div>
       </Combobox>
